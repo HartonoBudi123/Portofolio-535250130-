e2933eea505815fb992a59f7f6be6e888a1b57b7
HartonoBudi123 <hartonoobudii30@gmail.com>
2026-09-25 23:39:55 +0700

    fix: deploy

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-// Fungsionalitas toggle hamburger bar responsif
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('active');
     navbar.classList.toggle('active');
